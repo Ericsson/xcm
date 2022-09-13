bbda97042ba2aaa0c67b5e28b0301b44249ccd45
Mattias Rönnblom <mattias.ronnblom@ericsson.com>
2022-09-13 18:36:52 +0200

    Update meta data for release 1.6.2
    Signed-off-by: Mattias Rönnblom <mattias.ronnblom@ericsson.com>

--- include/xcm.h
+++ include/xcm.h
@@ -27,7 +27,7 @@ extern "C" {
  *
  * @author Mattias Rönnblom
  * @version 0.21 [API]
- * @version 1.6.1 [Implementation]
+ * @version 1.6.2 [Implementation]
  *
  * The low API/ABI version number is a result of all XCM releases
  * being backward compatible, and thus left the major version at 0.
