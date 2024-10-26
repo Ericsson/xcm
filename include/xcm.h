02eaa59b662bd38343d45a70d79b80afd6320a31
Mattias Rönnblom <mattias.ronnblom@ericsson.com>
2024-10-26 08:02:47 +0200

    Update meta data for 1.10.1 release
    Signed-off-by: Mattias Rönnblom <mattias.ronnblom@ericsson.com>

--- include/xcm.h
+++ include/xcm.h
@@ -27,7 +27,7 @@ extern "C" {
  *
  * @author Mattias Rönnblom
  * @version 0.25 [API]
- * @version 1.10.0 [Implementation]
+ * @version 1.10.1 [Implementation]
  *
  * The low API/ABI version number is a result of all XCM releases
  * being backward compatible, and thus left the major version at 0.
