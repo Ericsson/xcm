e60cfeb3575658dbc7ad92613fdc166f16ddc558
Mattias Rönnblom <mattias.ronnblom@ericsson.com>
2025-05-15 09:09:44 +0200

    Update meta data for release 1.11.2
    Update autoconf and header files with new patch version.
    
    Signed-off-by: Mattias Rönnblom <mattias.ronnblom@ericsson.com>

--- include/xcm.h
+++ include/xcm.h
@@ -27,7 +27,7 @@ extern "C" {
  *
  * @author Mattias Rönnblom
  * @version 0.26 [API]
- * @version 1.11.1 [Implementation]
+ * @version 1.11.2 [Implementation]
  *
  * The low API/ABI version number is a result of all XCM releases
  * being backward compatible, and thus left the major version at 0.
